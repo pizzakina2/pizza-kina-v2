0decf79b3c2cb14507bdeb9913faf3ade65e5afd
Pizzakina2 <pizzakina2@gmail.com>
2026-03-26 20:53:10 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBWOEXNkWxxQjGlSFeUB1sH_3IwPuR0JtY",
+  apiKey: "AIzaSyCesDlBgm87wKLmOpOCVyMWTm59njJWHOo",
   authDomain: "pizza-kina-v2.firebaseapp.com",
   projectId: "pizza-kina-v2",
   storageBucket: "pizza-kina-v2.firebasestorage.app",
   messagingSenderId: "1012440833599",
-  appId: "1:1012440833599:web:f7f39eb0325fe363c4a28a"
+  appId: "1:1012440833599:web:726101c745b16753c4a28a"
 };
 
 export const ADMIN_EMAIL = "pizzakina2@gmail.com";
--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBWOEXNkWxxQjGlSFeUB1sH_3IwPuR0JtY",
+  apiKey: "AIzaSyCesDlBgm87wKLmOpOCVyMWTm59njJWHOo",
   authDomain: "pizza-kina-v2.firebaseapp.com",
   projectId: "pizza-kina-v2",
   storageBucket: "pizza-kina-v2.firebasestorage.app",
   messagingSenderId: "1012440833599",
-  appId: "1:1012440833599:web:f7f39eb0325fe363c4a28a",
+  appId: "1:1012440833599:web:726101c745b16753c4a28a",
 };
 
 export const app = initializeApp(firebaseConfig);
